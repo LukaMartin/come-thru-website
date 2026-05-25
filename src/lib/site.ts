af11757c9ca5b8c7cc23914bac7b7e1f00b5880a
LukaMartin <ldm3334@gmail.com>
2026-05-25 14:46:28 +1000

    Update instagram URL

--- src/lib/site.ts
+++ src/lib/site.ts
@@ -2,6 +2,6 @@ export const CONTACT_EMAIL = "info@comethru.com.au";
 export const SUPPORT_EMAIL = "support@comethru.com.au";
 
 export const socialLinks = {
-  instagram: "https://www.instagram.com/c0me_thru",
+  instagram: "https://www.instagram.com/comethru.au",
   soundcloud: "https://on.soundcloud.com/VncJCWgzyLEhqKJO0Q",
 } as const;
