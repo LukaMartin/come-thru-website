f6ba6c2a0ab4fe7fc91208d1453330977ac43997
LukaMartin <ldm3334@gmail.com>
2026-08-31 09:20:49 +1000

    Upgrae packages to fix npm audit issues

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,6 +2,8 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
+    const { EventEmitter } = await import("node:events");
+    EventEmitter.defaultMaxListeners = 20;
     await import("../sentry.server.config");
   }
 
